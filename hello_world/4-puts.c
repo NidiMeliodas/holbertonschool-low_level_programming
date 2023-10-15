0e59675dbbe98ec6b646c78d1fa64f942ba419e7
NidiMeliodas <nidigashan@gmail.com>
2023-10-14 19:32:48 -0700

    sending updates

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-/**
-*description
-*/
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
