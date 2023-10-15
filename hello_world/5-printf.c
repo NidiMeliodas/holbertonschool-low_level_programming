0f0ea57819bdb1d10d5c9f4c1590e925d7565aae
NidiMeliodas <nidigashan@gmail.com>
2023-10-14 19:29:07 -0700

    sending updates

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-	/**
-         * This is the preferred style for multi-line
-         * comments in C source code.
-         * Please use it consistently.
-         *
-         * Description:  A column of asterisks on the left side,
-         * with beginning and ending almost-blank lines.
-         */
+/**
+*description
+*main = Program
+*return =0 
+*/
+
+
 
 int main(void)
 {
