aa44f049ac25b22433c0c147a57a76c58418645e
NidiMeliodas <nidigashan@gmail.com>
2023-10-14 19:26:20 -0700

    sending updates

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-	/**
-         * This is the preferred style for multi-line
-         * comments in C source code.
-         * Please use it consistently.
-         *
-         * Description:  A column of asterisks on the left side,
-         * with beginning and ending almost-blank lines.
-         */
+/**
+*description
+*/
 
 int main(void)
 {
