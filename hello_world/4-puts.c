d9c2436c1ed99f3ec0ab908b30ed964fed4f11d6
NidiMeliodas <nidigashan@gmail.com>
2023-10-14 19:21:53 -0700

    sending updates

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 int main(void)
 {
-	/* Use this */
+	/**
+	 * This is the preferred style for multi-line
+	 * comments in C source code.
+	 * Please use it consistently.
+	 *
+	 * Description:  A column of asterisks on the left side,
+	 * with beginning and ending almost-blank lines.
+	 */
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 int main(void)
 {
-	/* Use this */
+	/**
+	 * This is the preferred style for multi-line
+	 * comments in C source code.
+	 * Please use it consistently.
+	 *
+	 * Description:  A column of asterisks on the left side,
+	 * with beginning and ending almost-blank lines.
+	 */
+
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
