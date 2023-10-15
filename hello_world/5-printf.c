db471e827a83336e083272d63e83510b405763ae
NidiMeliodas <nidigashan@gmail.com>
2023-10-14 19:49:45 -0700

    sending updates

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-* main - Program
-*
-* return : 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
