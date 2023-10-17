ae5afc20f42e8611b64167c1403ecbe40042ce82
NidiMeliodas <nidigashan@gmail.com>
2023-10-17 05:04:10 -0700

    sending updates

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,16 @@ int main(void)
 {
 	char c;
 	char q ='q',e='e';
+	
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		if (c =! q && c =! e)
+		if (c != q && c != e)
 		{
 			putchar(c);
 		}
 	}
+
 	putchar('\n');
+	
 	return (0);
 }
