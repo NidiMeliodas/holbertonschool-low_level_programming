49d8b15025a1590bcd8ec2dd7282917975f33123
NidiMeliodas <nidigashan@gmail.com>
2023-10-17 01:37:48 -0700

    sending updates

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Sucess)
+ *
+ */
+int main(void)
+{
+	for ( c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	
+	for ( c = 'A' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
