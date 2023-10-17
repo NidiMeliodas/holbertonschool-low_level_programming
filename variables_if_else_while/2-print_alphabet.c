6c12a19a3ea0d72c5e203b713fcb60d96c6e5be0
NidiMeliodas <nidigashan@gmail.com>
2023-10-17 01:29:11 -0700

    sending updates

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -4,17 +4,17 @@
  * main - Entry point
  *
  * Return: Always 0 (Sucess)
- * 
+ *
  */
 int main(void)
 {
 	char c;
 
-	for(c = 'a' ; c <= 'z' ; c++)
+	for (c = 'a' ; c <= 'z' ; c++)
 	{
 		putchar(c);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 
 }
