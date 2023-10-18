282977c6b249f18b95e2b7f94ba9c7c01bb1b181
NidiMeliodas <nidigashan@gmail.com>
2023-10-18 06:49:48 -0700

    sending updates

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,1 +1,22 @@
+#include "main.h"
 
+/**
+ * main - void print_alphabet print 10 times the alphabet in lowercase,
+ *
+ *followed by a new
+ */
+
+void print_alphabet(void)
+{
+	char a;
+	int  i;
+
+	for (i = 0, i < 10, i++)
+	{
+		for (a = 'a', a <= 'z',a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n);
+	}
+}
