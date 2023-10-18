1c17dd852d735e67ad1faf93c82d883eb4105f7b
NidiMeliodas <nidigashan@gmail.com>
2023-10-18 06:38:30 -0700

    sending updates

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,14 +1,1 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
 int _putchar(char c);
-
-#endif
