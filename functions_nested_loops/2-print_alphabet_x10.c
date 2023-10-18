faf7a3e6073c27e365773ae9aff012d2f2e46c41
NidiMeliodas <nidigashan@gmail.com>
2023-10-18 07:09:19 -0700

    sending updates

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,12 @@ void print_alphabet_x10(void)
 {
         char a;
 	int i;
-        for (i = 0; i < 10; i++)
-        {
-                for (a = 'a'; a <= 'z'; a++)
-                {
-                        _putchar(a);
-                }
-                _putchar('\n');
-        }
+	for (i = 0; i < 10; i++){
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
 }
 
