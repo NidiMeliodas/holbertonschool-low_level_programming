95aa70af783e9cdf8e7eccdc01d71fb9699bc8a6
NidiMeliodas <nidigashan@gmail.com>
2023-10-18 06:36:07 -0700

    sending updates

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,8 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-#include <unistd.h>
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -10,9 +8,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+
+int _putchar(char c);
 
 #endif
