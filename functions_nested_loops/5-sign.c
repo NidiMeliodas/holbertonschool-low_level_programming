0c7cfef23470e6625c7e89064ca876a6fc84785c
NidiMeliodas <nidigashan@gmail.com>
2023-10-19 06:49:07 -0700

    sending updates

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_sign - Checks if the number is positive or negative 
+ * print_sign - Checks if the number is positive or negative
  * @n: the int to check
  * Return: 1 and + if n greater than 0, 0 if n = 0, -1 if n less than 0
  */
@@ -13,12 +13,12 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-       	else if (n == 0)
+	else if (n == 0)
 	{
 		_putchar(48);
 		return (0);
 	}
-       	else if (n < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 	}
