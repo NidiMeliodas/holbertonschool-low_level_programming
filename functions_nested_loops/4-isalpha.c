e10ffe29713a5ccf6525c481ecea68f7249a7437
NidiMeliodas <nidigashan@gmail.com>
2023-10-19 06:34:01 -0700

    sending updates

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int _isalpha - Chechs letters
+ * @c: the checked character
+ * Return: 1 if c = letter ,else 0 
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
