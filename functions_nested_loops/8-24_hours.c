29b19ba275c87411e3eeea71f5421706c30b4fbc
NidiMeliodas <nidigashan@gmail.com>
2023-10-19 14:41:29 -0700

    sending updates

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every min of his day
+ * starting from 00:00 to 23:59
+ */
+
+void jack_bauer(void)
+{
+	for (h = 0; h < 24; h++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((h/10)+'0');
+			_putchar((h%10)+'0');
+			_putchar(':');
+			_putchar((min/10)+'0');
+			_putchar((min%10)+'0');
+			_putchar('\n')
+		}
+	}
+}
