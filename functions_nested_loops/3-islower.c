3fc361033efb1c9860aa213ed3e46e4bd1fc9b2d
NidiMeliodas <nidigashan@gmail.com>
2023-10-18 21:58:15 -0700

    sending updates

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,1 +1,19 @@
+#include "main.h"
 
+/**
+ * _islower - Check if c is lowercase c to return 1 if not return 0
+ *
+ * Return: 1 or 0
+ */
+int _islower(int c)
+{
+	if ( c == 'c')
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+
+}
