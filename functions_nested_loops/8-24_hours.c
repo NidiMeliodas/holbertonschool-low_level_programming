d5c9470620a1a51c348d0b6be8524e95ca581a9f
NidiMeliodas <nidigashan@gmail.com>
2023-10-19 14:47:38 -0700

    sending updates

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,17 +7,17 @@
 
 void jack_bauer(void)
 {
-	int h,min;
+	int h, min;
 
 	for (h = 0; h < 24; h++)
 	{
 		for (min = 0; min < 60; min++)
 		{
-			_putchar((h/10)+'0');
-			_putchar((h%10)+'0');
+			_putchar((h / 10) +'0');
+			_putchar((h % 10) +'0');
 			_putchar(':');
-			_putchar((min/10)+'0');
-			_putchar((min%10)+'0');
+			_putchar((min / 10) +'0');
+			_putchar((min % 10) +'0');
 			_putchar('\n');
 		}
 	}
