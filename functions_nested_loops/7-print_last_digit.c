32da437bc67fbbc4f9059b534372503c0b9d58c8
NidiMeliodas <nidigashan@gmail.com>
2023-10-19 08:48:11 -0700

    sending updates

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,15 +9,16 @@
 int print_last_digit(int n)
 {
 	int a;
+	
 	if (n < 0)
 	{
-		n=-n;
+		n = -n;
 	}
-	a=n%10;
+	a = n % 10;
 	if (a < 0)
 	{
-		a=-a;
+		a = -a;
 	}
-	_putchar(a+'0');
+	_putchar(a +'0');
 	return (a);
 }
