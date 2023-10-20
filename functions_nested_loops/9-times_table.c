e99fdaa79e43498740165e53916e0ac2c2fb8145
NidiMeliodas <nidigashan@gmail.com>
2023-10-20 06:28:43 -0700

    sending updates

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -29,17 +29,17 @@ void times_table(void)
 			
 			if ( d == 0 )
 			{
+				_putchar(u + '0');
 				_putchar(44);
 				_putchar(32);
-				_putchar(u + '0');
 			}
 			else if (d != 0)
 			{
+				_putchar(d +'0');
+				_putchar(u +'0');
 				_putchar(44);
 				_putchar(32);
 				_putchar(32);
-				_putchar(d+'0');
-				_putchar(u+'0');
 			}
 		}
 		_putchar('\n');
