65ef4d5db9962df824fa264c29443477144e4df4
NidiMeliodas <nidigashan@gmail.com>
2023-10-20 01:45:57 -0700

    sending updates

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -3,19 +3,32 @@
 /**
  * times_table - Prints the 9 times table starting with 0.
  *
- * Return: Always 0 
+ * Return: Always 0
+ *
+ * c = colonne
+ * l = ligne
+ * r = resultat
+ *
  */
 
 void times_table(void)
 {
 	int c;
+	int l;
+	int r;
 
 	for (c = 0; c < 10; c++)
 	{
 		_putchar(0 + '0');
-		_putchar(44);
-		_putchar(32);
-		_putchar(32);
+		for (l = 0; l <= 9;l++)
+		{
+			r=c*l;
+			_putchar((r/10)+'0');
+			_putchar((r%10)+'0');
+			_putchar(44);
+			_putchar(32);
+			_putchar(32);
+		}
 		_putchar('\n');
 	}
 
