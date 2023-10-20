440765057a437f664e9f78bb368eed94e783efd8
NidiMeliodas <nidigashan@gmail.com>
2023-10-20 14:06:36 -0700

    sending updates

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+  * add - Adds two values of n1 and n2
+  * @n1: The first number to be computed
+  *
+  * @n2: The second number to be computed
+  *
+  * Return: Integer value
+  */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
