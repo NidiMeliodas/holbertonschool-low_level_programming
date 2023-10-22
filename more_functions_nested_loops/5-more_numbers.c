e0ef332036744cb482dcd7d1f541df55ccd84c9f
NidiMeliodas <nidigashan@gmail.com>
2023-10-21 18:33:38 -0700

    sending updates

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,14 +14,8 @@ void more_numbers(void)
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-			{
-				_putchar(j / 10 + '0');			
-				_putchar(j % 10 + '0');
-			}
-			else
-			{
-				_putchar(j % 10 + '0');
-			}
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
