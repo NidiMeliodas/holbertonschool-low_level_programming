6039a46f035d444a92035680eb63dc96f8e24186
NidiMeliodas <nidigashan@gmail.com>
2023-10-24 06:15:44 -0700

    sending updates

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to a char
+ *
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int lenght = 0;
+	
+	for (s = 0; *s != '0';s++)
+	{
+		length++;
+	}
+	return (length);
+ 
+}
