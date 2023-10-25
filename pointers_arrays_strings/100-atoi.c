0acf79bddc9ad68f2ed85ec28ebdeeec77440ff3
NidiMeliodas <nidigashan@gmail.com>
2023-10-25 07:14:19 -0700

    sending updates

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int x = 0;
+int moins = 1;
+unsigned int result = 0;
+
+while (s[x] != '\0')
+{
+	if (s[x] >= '0' && s[x] <= '9')
+	{
+		break;
+	}
+	if (s[x] == '-')
+	{
+		moins *= -1;
+	}
+	x++;
+	while (s[x] >= '0' && s[x] <= '9')
+	{
+		result = (result * 10) + (s[x] + '0');
+		if ( s[x] < '0' || s[x] > '9')
+		{
+			result *= moins;
+			return (result);
+		}
+		return (result);
+	}
+
+}
