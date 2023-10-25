f42bdc00f013eeb742f79fe5777ac3b31d94c9f2
NidiMeliodas <nidigashan@gmail.com>
2023-10-25 07:24:09 -0700

    sending updates

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
-int x = 0;
-int moins = 1;
-unsigned int result = 0;
-
 int _atoi(char *s)
 {
+	int x = 0;
+	int moins = 1;
+	unsigned int result = 0;
+
+
 	while (s[x] != '\0')
 	{
 		if (s[x] >= '0' && s[x] <= '9')
@@ -15,17 +16,17 @@ int _atoi(char *s)
 		if (s[x] == '-')
 		{
 			moins *= -1;
+			x++;
 		}
-		x++;
-		while (s[x] >= '0' && s[x] <= '9')
+	}
+	while (s[x] >= '0' && s[x] <= '9')
+	{
+		result = (result * 10) + (s[x] + '0');
+		if ( s[x] < '0' || s[x] > '9')
 		{
-			result = (result * 10) + (s[x] + '0');
-			if ( s[x] < '0' || s[x] > '9')
-			{
-				result *= moins;
-				return (result);
-			}
+			result *= moins;
 			return (result);
 		}
 	}
+	return (result);
 }
