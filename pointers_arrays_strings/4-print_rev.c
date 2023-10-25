f7901b0ada1b71ad36e9acbed6f5d93fb7c54dc7
NidiMeliodas <nidigashan@gmail.com>
2023-10-25 05:37:12 -0700

    sending updates

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: The string to print
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int lenght;
+	int i;
+
+	while (*(s+lenght) != '\0')
+	{
+		lenght++
+	}
+	for (i = lenght; i >= 0;i--)
+	{
+		_putchar(*s+lenght);
+	}
