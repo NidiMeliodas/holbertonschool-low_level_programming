a3a47b047d7bac14033c9181abe0f567f9f5893a
NidiMeliodas <nidigashan@gmail.com>
2023-10-26 21:57:33 -0700

    sending updates

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,20 @@
 #include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: string to print
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+	{
+		if (i % 2 == 0)
+			_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *puts_half - function that prints half of a string, followed by a new line.
+ *
+ *@str: This is the input string
+ */
+void puts_half(char *str)
+{
+	int i, half;
+
+	i= 0;
+	while (str[i] != '\0')
+		i++;
+
+	half = i / 2;
+
+	if (i % 2 == 1)
+		half++;
+
+	while (half < i)
+	{
+		_putchar(str[half]);
+		half++;
+	}
+	_putchar('\n');
+}
