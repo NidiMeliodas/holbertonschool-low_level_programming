34041750fea25b3aa8f0c48b9d9536c7c90b83f0
NidiMeliodas <nidigashan@gmail.com>
2023-10-27 06:11:01 -0700

    sending updates

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,13 +9,13 @@
 
 char *_strcpy(char *dest, char *src)
 {
-  int i;
+  int i = 0;
   
   while (src[i])
   {
     dest[i] = src[i];
     i++;
   }
-  dest = '\0';
+  dest[i] = '\0';
   return (dest);
 }
