c66769e4746191ff8edd776c04a8ac8f6399c216
NidiMeliodas <nidigashan@gmail.com>
2023-10-29 07:28:29 -0700

    sending updates

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcat - links two strings
+ *
+ * @dest:
+ *
+ * @src: 
+ *
+ * Return: pointer string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+	char desti = (*(dest + i));
+	char srcj = (*(src + j));
+
+	while (desti)
+	{
+		i++;
+	}
+
+	while (desti  = srcj)
+	{
+		i++;
+		j++;
+	}
+
+	return (dest);
+}
--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,13 +9,13 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
+  int i = 0;
+  
+  while (src[i])
+  {
+    dest[i] = src[i];
+    i++;
+  }
+  dest[i] = '\0';
+  return (dest);
 }
