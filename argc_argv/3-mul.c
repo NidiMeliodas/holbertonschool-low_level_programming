2d83f5c163625b7624ccb99e92ffabb933f191e7
NidiMeliodas <nidigashan@gmail.com>
2023-11-03 13:31:01 -0700

    f

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,14 +11,17 @@
 
 int main(int argc, char *argv[])
 {
+	int num1, num2, result;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return 1;
 	}
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int result = num1 * num2;
+	
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
 	
 	printf("%d\n", result);
 	
