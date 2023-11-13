e94d12f90ac312a478b91be62666976117566eea
NidiMeliodas <nidigashan@gmail.com>
2023-11-13 03:06:50 -0800

    sending updates

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Short description
+ * @name: prenom
+ * @age: l'age
+ * @owner: proprio
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
