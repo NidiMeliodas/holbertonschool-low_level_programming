9c856bfe2127532171e3b7ce303121d3a5a4181d
NidiMeliodas <nidigashan@gmail.com>
2023-11-17 14:36:10 -0800

    sending updates

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stddef.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+
+#endif
