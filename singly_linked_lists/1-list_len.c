6ba3f6fe746dc6b5d3e6a5937071b12586b7e534
NidiMeliodas <nidigashan@gmail.com>
2023-12-01 13:15:19 -0800

    Fix 1-list_len.c

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -8,13 +8,16 @@
  *
  * Return: The number of elements in the linked list.
  */
-
 size_t list_len(const list_t *h)
 {
-	int i;
+	size_t count = 0;
 
-	for (i = 0; h != NULL; i++)
-		break;
-	h = h->next;
-	return (i);
+	while (h != NULL)
+	{
+		count++;
+
+		h = h->next;
+	}
+
+	return (count);
 }
