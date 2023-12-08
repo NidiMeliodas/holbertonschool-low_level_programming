9c7a9a3c0919db1b583586047f82fc95a3ea2d1b
NidiMeliodas <nidigashan@gmail.com>
2023-12-07 17:52:04 -0800

    Task 3 fixed ?

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -10,11 +10,13 @@
  * @fd: File descriptor to close.
  * @filename: Name of the file associated with the file descriptor.
  */
-void closeFile(int fd, const char *filename) {
-    if (close(fd) == -1) {
-        dprintf(STDERR_FILENO, "Error: Can't close fd for %s\n", filename);
-        exit(100);
-    }
+void closeFile(int fd, const char *filename)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd for %s\n", filename);
+		exit(100);
+	}
 }
 
 /**
@@ -23,48 +25,76 @@ void closeFile(int fd, const char *filename) {
  * @dest: File descriptor of the destination file.
  * @av: Command-line arguments.
  */
-void copyFile(int src, int dest, char **av) {
-    char buffer[BUFFER_SIZE];
-    ssize_t bytesRead, bytesWritten;
 
-    while ((bytesRead = read(src, buffer, BUFFER_SIZE)) > 0) {
-        bytesWritten = write(dest, buffer, bytesRead);
-        if (bytesWritten == -1) {
-            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-            exit(99);
-        }
-    }
+void copyFile(int src, int dest, char **av)
+{
+	char buffer[BUFFER_SIZE];
+	ssize_t bytesRead, bytesWritten;
 
-    if (bytesRead == -1) {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-        exit(98);
-    }
+	while ((bytesRead = read(src, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytesWritten = write(dest, buffer, bytesRead);
+		if (bytesWritten == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	}
+
+	if (bytesRead == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 }
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ *
+ * Description:
+ *   - Validates the number of command-line arguments.
+ *   - Opens the source file in read-only mode.
+ *   - Handles errors if unable to open the source file.
+ *   - Opens or creates the destination file in write-only
+ *   - mode, truncating if it exists.
+ *   - Handles errors if unable to open or create the destination file.
+ *   - Copies the content from the source file to the destination file.
+ *   - Closes the source and destination files.
+ *
+ * Return: Always 0 (indicating successful execution).
+ */
+int main(int argc, char **argv)
+{
+	int src;
+	int dest;
 
-int main(int argc, char **argv) {
-    if (argc != 3) {
-        dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-        exit(97);
-    }
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
 
-    int src = open(argv[1], O_RDONLY, 0664);
-    if (src == -1) {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
+	src = open(argv[1], O_RDONLY, 0664);
+	if (src == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
-    int dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-    if (dest == -1) {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-        closeFile(src, argv[1]);
-        exit(99);
-    }
+	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (dest == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		closeFile(src, argv[1]);
+		exit(99);
+	}
 
-    copyFile(src, dest, argv);
+	copyFile(src, dest, argv);
 
-    closeFile(src, argv[1]);
-    closeFile(dest, argv[2]);
+	closeFile(src, argv[1]);
+	closeFile(dest, argv[2]);
 
-    return 0;
+	return (0);
 }
 
